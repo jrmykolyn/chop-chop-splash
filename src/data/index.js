2de884f67ef99a0688d0afdc46d3ad1d61076e41
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-08-25 22:38:39 -0400

    Add 'src/' dir.

---
+++ src/data/index.js
@@ -0,0 +1,116 @@
+module.exports = {
+    contactInfo: {
+        address: '771 Dundas Street West',
+        phoneNumber: {
+            display: '416.842.8277',
+            raw: 4168428277
+        },
+        website: {
+            display: 'chop-chop.ca',
+            raw: 'http://chop-chop.ca'
+        },
+    },
+    menuItems: [
+        {
+            type: '餃 Dumplings',
+            dishes: [
+                {
+                    nameZh: '豬肉韭菜水餃',
+                    nameEn: 'Boiled Pork & Chive (12pc)',
+                    price: 7,
+                },
+                {
+                    nameZh: '豬肉蔬菜水餃',
+                    nameEn: 'Boiled Pork and Vegetable (12pc)',
+                    price: 7,
+                },
+                {
+                    nameZh: '豬肉韭菜煎餃',
+                    nameEn: 'Boiled Pork and Vegetable (12pc)',
+                    price: 9,
+                },
+                {
+                    nameZh: '豬肉蔬菜煎餃',
+                    nameEn: 'Pan-fried Pork and Vegetable (8pc)',
+                    price: 9,
+                },
+            ],
+        },
+        {
+            type: '小食 Appetizers',
+            dishes: [
+                {
+                    nameZh: '素春卷',
+                    nameEn: 'Vegetarian Spring Rolls (2pc)',
+                    price: 4
+                },
+                {
+                    nameZh: '蔥油餅',
+                    nameEn: 'Pan-fried Green Onion Pancake',
+                    price: 5
+                },
+                {
+                    nameZh: '台式酸辣湯',
+                    nameEn: 'Hot & Sour Soup',
+                    price: 5
+                },
+                {
+                    nameZh: '炸豆腐',
+                    nameEn: 'Deep-fried Tofu w/ Garlic Sauce',
+                    price: 5
+                },
+                {
+                    nameZh: '蜜汁雞翼',
+                    nameEn: 'Honey-glazed Wings (2pc)',
+                    price: 5
+                },
+                {
+                    nameZh: '凉拌黄瓜',
+                    nameEn: 'Spicy Cucumber Salad',
+                    price: 6
+                },
+            ],
+        },
+        {
+            type: '主菜 Mains',
+            dishes: [
+                {
+                    nameZh: '乾煸四季豆',
+                    nameEn: 'Sautéed Green Beans',
+                    price: 8,
+                },
+                {
+                    nameZh: '台式紅燒牛肉麵',
+                    nameEn: 'Taiwanese Braised Beef Noodle Soup',
+                    price: 10,
+                },
+                {
+                    nameZh: '椒鹽鮮魷',
+                    nameEn: 'Salt and Pepper Fried Squid',
+                    price: 12,
+                },
+                {
+                    nameZh: '上等叉燒肉',
+                    nameEn: 'Char Siu BBQ Pork',
+                    price: 12,
+                },
+                {
+                    nameZh: '姜葱牛肉',
+                    nameEn: 'Ginger Beef',
+                    price: 12,
+                },
+                {
+                    nameZh: '台式炒米粉',
+                    nameEn: 'Fried Vermicelli w/ Shrimp, Shredded Pork & Egg',
+                    price: 12,
+                },
+                {
+                    nameZh: '台式鹽酥雞',
+                    nameEn: 'Taiwanese Basil Chicken',
+                    price: 13,
+                },
+            ],
+        },
+    ],
+    tagline: '"Less hungry, more happy."'
+};
